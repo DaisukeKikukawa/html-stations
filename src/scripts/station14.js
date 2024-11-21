eb7939f4ad3acd5cb24c6d3cc6cc2356d102446e
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2024-11-21 09:41:37 +0900

    station:14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -7,12 +7,22 @@ function getData() {
         { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
     ];
     return test.map(buildFullName)
+
+    // return test.map((data) => {
+    //   const newData = { ...data };
+    //   newData.full_name = newData.family_name + " " + newData.first_name;
+    //   return newData;
+    // });
 }
 
 function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    const newData = { ...data };
+    // full_nameプロパティを追加（family_name + 半角スペース + first_name）
+    newData.full_name = newData.family_name + " " + newData.first_name;
+    return newData;
 }
 
 // 出力が想定される答え
